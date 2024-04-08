3b800615e32ae64441cb7d497f278687a3aaca84
DespaC2 <eric1figueredo@outlook.com>
2024-04-08 09:29:47 +0200

    cart

---
+++ MajornaGameStore/MajornaGameStore.Shared/Dtos/CartItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MajornaGameStore.Shared.Dtos;
+
+public class CartItemDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public double Price { get; set; }
+
+    [Required, MinLength(1)] public int Quantity { get; set; } = 1;
+
+    public string Image { get; set; } 
+}

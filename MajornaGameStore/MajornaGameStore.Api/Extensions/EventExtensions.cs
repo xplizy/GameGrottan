4fac8984d5140f69abc4c4f9eb5f2eab2afadeec
DespaC2 <eric1figueredo@outlook.com>
2024-04-09 10:01:17 +0200

    Events start

---
+++ MajornaGameStore/MajornaGameStore.Api/Extensions/EventExtensions.cs
@@ -0,0 +1,48 @@
+﻿using MajornaGameStore.DataAccess.Services;
+using Microsoft.Identity.Client;
+
+namespace MajornaGameStore.Api.Extensions;
+
+public static class EventExtensions
+{
+    public static IEndpointRouteBuilder MapEventEndPoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/events");
+
+
+        group.MapGet("/", GetAllEventsAsync);
+        group.MapGet("/{id}", GetEventsByIdAsync);
+        group.MapPut("/{id}", UpdateEvent);
+        return app;
+    }
+
+    public static async Task<IResult> GetAllEventsAsync(EventService eventService)
+    {
+
+        var events = await eventService.GetAllAsync();
+        return Results.Ok(events);
+
+    }
+
+    public static async Task<IResult> GetEventsByIdAsync(EventService eventService, int id)
+    {
+
+        var events = await eventService.GetByIdAsync(id);
+
+        if (events is null)
+            return Results.NotFound();
+        
+        return Results.Ok(events);
+
+    }
+
+    public static async Task<IResult> UpdateEvent(EventService eventService, int id)
+    {
+        var events = await eventService.UpdateAsync();
+
+        if (events is null)
+            return Results.NotFound();
+
+        return Results.Ok(events);
+    }
+}
--- MajornaGameStore/MajornaGameStore.DataAccess/Services/EventService.cs
+++ MajornaGameStore/MajornaGameStore.DataAccess/Services/EventService.cs
@@ -1,9 +1,11 @@
 ﻿using MajornaGameStore.DataAccess.Entities;
 using MajornaGameStore.DataAccess.Sql.Repositories;
+using MajornaGameStore.Shared.Interfaces;
 
 namespace MajornaGameStore.DataAccess.Services;
 
-public class EventService(EventRepository repository) : ServiceBase<Event, int>(repository)
+public class EventService(IEventRepository eventRepository) : ServiceBase<Event, int>(eventRepository)
 {
-    
+    private readonly IEventRepository _eventRepository = eventRepository;
+
 }

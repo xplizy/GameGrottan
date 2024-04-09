10221261e16c4d0a6e170fd147f312218e3fc5e5
Viv-dn <Vivian96@live.se>
2024-04-09 15:26:07 +0200

    added checkout back and frontend

--- MajornaGameStore/MajornaGameStore.Api/Program.cs
+++ MajornaGameStore/MajornaGameStore.Api/Program.cs
@@ -1,5 +1,6 @@
 using MajornaGameStore.DataAccess.Sql;
 using Microsoft.EntityFrameworkCore;
+using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,32 @@ builder.Services.AddDbContext<MajornaDbContext>(
     options =>
         options.UseSqlServer(connectionString));
 
+StripeConfiguration.ApiKey = builder.Configuration["Stripe:SecretKey"];
+
+//add cors for no errors later on
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddControllers();
+
+//StripeConfiguration.ApiKey = "sk_test_51P1o0CALQne3zawOR30h5V9cqOtm7GK7l4t5HA6jHdVlkg8tyBiiqzjmUI6prlXhArha19lUxUq3jEBp9Ro531nH00bgxGxZqv";
+
+//var options = new PaymentIntentCreateOptions
+//{
+//    Amount = 500,
+//    Currency = "gbp",
+//    PaymentMethod = "pm_card_se",
+//};
+//var service = new PaymentIntentService();
+//service.Create(options);
 
 var app = builder.Build();
 
@@ -23,6 +50,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//enligt instruktioner so that asp.net can activate these services
+app.UseCors();
+app.UseRouting();
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
---
+++ MajornaGameStore/MajornaGameStore.Shared/CheckoutOrderResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MajornaGameStore.Shared;
+
+public class CheckoutOrderResponse
+{
+    //each payment process in STRIPE called Session and has ID + annan info -> får gratis
+    public string? SessionId { get; set; }
+
+    public string? PubKey { get; set; }
+}

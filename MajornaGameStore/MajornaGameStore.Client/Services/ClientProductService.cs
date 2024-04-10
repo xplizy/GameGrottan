905cce8b2e338144495aac6aaac022e8c223806c
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-04-10 15:54:08 +0200

    Implemented ProductViewModel, and code to fetch products from the API

--- MajornaGameStore/MajornaGameStore.Client/Services/ClientProductService.cs
+++ MajornaGameStore/MajornaGameStore.Client/Services/ClientProductService.cs
@@ -1,6 +1,42 @@
-﻿namespace MajornaGameStore.Client.Services;
+﻿using System.Net.Http.Json;
+using MajornaGameStore.Shared.Dtos;
+using MajornaGameStore.Shared.Interfaces;
 
-public class ClientProductService
+namespace MajornaGameStore.Client.Services;
+
+public class ClientProductService(HttpClient httpClient) : IClientProductService
 {
-    
+    private readonly HttpClient _httpClient = httpClient;
+    public async Task<ICollection<ProductDto>> GetAllAsync()
+    {
+        var response = await _httpClient.GetAsync($"/products");
+
+        if (response.IsSuccessStatusCode == false)
+            return new List<ProductDto>();
+
+        var result = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+
+        return result.ToList();
+
+    }
+
+    public Task<ProductDto?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductDto> AddAsync(ProductDto entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(ProductDto entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
 }

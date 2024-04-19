3b8defa971c61f4e2d0cf29307635976a4cc345e
Viv-dn <Vivian96@live.se>
2024-04-19 14:38:57 +0200

    fixed alignment and margin in product page

--- MajornaGameStore/MajornaGameStore.Client/Program.cs
+++ MajornaGameStore/MajornaGameStore.Client/Program.cs
@@ -13,9 +13,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://majornaggapi.azurewebsites.net") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://majornaggapi.azurewebsites.net") });
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7190") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7190") });
 
 builder.Services
     .AddScoped<ProductViewModel>()

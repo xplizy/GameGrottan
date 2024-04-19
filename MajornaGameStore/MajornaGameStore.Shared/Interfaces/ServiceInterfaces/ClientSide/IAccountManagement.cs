fa22731a039b0dd9b8df76a164a3b1013a319ba9
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-04-19 11:25:33 +0200

    Implemented classes necessary for authentication/authorization for frontend

---
+++ MajornaGameStore/MajornaGameStore.Shared/Interfaces/ServiceInterfaces/ClientSide/IAccountManagement.cs
@@ -0,0 +1,14 @@
+﻿using MajornaGameStore.Shared.Models.Identity;
+
+namespace MajornaGameStore.Shared.Interfaces.ServiceInterfaces.ClientSide;
+
+public interface IAccountManagement
+{
+    public Task<AuthResponseModel> LoginAsync(string email, string password);
+
+    public Task LogoutAsync();
+
+    public Task<AuthResponseModel> RegisterAsync(string email, string password);
+
+    public Task<bool> CheckAuthenticatedAsync();
+}

22cdd2b021ef93e95d475be7a6e466efa40d6114
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-04-19 10:57:26 +0200

    Secured endpoints

--- MajornaGameStore/MajornaGameStore.Api/Extensions/EventTypeExtension.cs
+++ MajornaGameStore/MajornaGameStore.Api/Extensions/EventTypeExtension.cs
@@ -11,7 +11,7 @@ public static class EventTypeExtension
         var group = app.MapGroup("/eventTypes");
 
 
-        group.MapPost("/", AddEventType).RequireAuthorization();
+        group.MapPost("/", AddEventType).RequireAuthorization(policy => policy.RequireRole("Administrator")); ;
         return app;
     }
 

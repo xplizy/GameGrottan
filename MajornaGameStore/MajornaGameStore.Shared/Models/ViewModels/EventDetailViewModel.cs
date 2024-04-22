d8a8d23231887f684c18714b63261a5aa6994135
DespaC2 <eric1figueredo@outlook.com>
2024-04-22 15:06:02 +0200

    Fixed bad code

--- MajornaGameStore/MajornaGameStore.Shared/Models/ViewModels/EventDetailViewModel.cs
+++ MajornaGameStore/MajornaGameStore.Shared/Models/ViewModels/EventDetailViewModel.cs
@@ -4,14 +4,43 @@ using MajornaGameStore.Shared.Interfaces.ServiceInterfaces.ClientSide;
 
 namespace MajornaGameStore.Shared.Models.ViewModels;
 
-public class EventDetailViewModel(IClientEventsService detailService) : ViewModelBase<EventDto, int>(detailService)
+public class EventDetailViewModel(IClientEventsService detailService, IClientCartService cartService) : ViewModelBase<EventDto, int>(detailService)
 {
     private readonly IClientEventsService _eventDetailService = detailService;
+    private readonly IClientCartService _cartService = cartService;
 
     public EventDto SelectedEvent { get; set; }
 
     public  async Task OnInite(int id)
     {
         SelectedEvent = await _eventDetailService.GetByIdAsync(id);
+    }
+
+    public async Task AddToCartAsync(EventDto eventDto, int quantity)
+    {
+        var cartItems = await cartService.GetAllAsync();
+        foreach (var item in cartItems)
+        {
+            if (item is CartTicketDto)
+            {
+                CartTicketDto cartTicket = (CartTicketDto)item;
+
+                if (cartTicket.EventId == eventDto.Id)
+                {
+                    item.Quantity += quantity;
+                    return;
+                }
+
+            }
+        }
+
+        var cartItem = new CartTicketDto
+        {
+            Name = eventDto.Name,
+            Price = eventDto.Price,
+            EventId = eventDto.Id,
+            Quantity = quantity
+        };
+        await cartService.AddAsync(cartItem);
     }
 }

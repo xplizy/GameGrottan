2592dfabae79f1051e87ed8303d236984f15eb68
Viv-dn <Vivian96@live.se>
2024-04-22 14:41:12 +0200

    Committing

--- MajornaGameStore/MajornaGameStore.Shared/Models/ViewModels/EventsViewModel.cs
+++ MajornaGameStore/MajornaGameStore.Shared/Models/ViewModels/EventsViewModel.cs
@@ -12,24 +12,9 @@ public class EventsViewModel(IClientEventsService eventService, IClientCartServi
     private readonly IClientEventsService _eventService = eventService;
     private readonly IClientCartService _cartService = cartService;
 
-    public async Task<int> TicketsCount(int eventId)
-    {
-        var cartItems = await cartService.GetAllAsync();
-        int ticketsCount = 0;
-
-        foreach (var item in cartItems)
-        {
-            if (item is CartTicketDto cartTicket && cartTicket.EventId == eventId)
-            {
-                ticketsCount += cartTicket.Quantity;
-            }
-        }
-        return ticketsCount;
-    }
 
     public async Task AddToCartAsync(EventDto eventDto, int quantity)
     {
-
         var cartItems = await cartService.GetAllAsync();
         foreach (var item in cartItems)
         {

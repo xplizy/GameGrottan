3af985fbf507b9450d3c645b90b40eec881267f8
Arwin Rashid <arwin.rashid@iths.se>
2024-04-23 17:43:26 +0000

    Updated zipArtifacts.ps1

--- zipArtifacts.ps1
+++ zipArtifacts.ps1
@@ -1,5 +1,10 @@
+param (
+    [string]$ArtifactStagingDirectory,
+    [string]$BuildConfiguration
+)
+
 # Zip .Api project artifacts
-Compress-Archive -Path "$env:BUILD_ARTIFACTSTAGINGDIRECTORY/MajornaGameStore/MajornaGameStore.Api/bin/$(BuildConfiguration)/*" -DestinationPath "$env:BUILD_ARTIFACTSTAGINGDIRECTORY/MajornaGameStore/MajornaGameStore.Api/api.zip" -Force
+Compress-Archive -Path "$ArtifactStagingDirectory/MajornaGameStore/MajornaGameStore.Api/bin/$BuildConfiguration/*" -DestinationPath "$ArtifactStagingDirectory/MajornaGameStore/MajornaGameStore.Api/api.zip" -Force
 
 # Zip .DataAccess project artifacts
-Compress-Archive -Path "$env:BUILD_ARTIFACTSTAGINGDIRECTORY/MajornaGameStore/MajornaGameStore.DataAccess/bin/$(BuildConfiguration)/*" -DestinationPath "$env:BUILD_ARTIFACTSTAGINGDIRECTORY/MajornaGameStore/MajornaGameStore.DataAccess/dataaccess.zip" -Force
+Compress-Archive -Path "$ArtifactStagingDirectory/MajornaGameStore/MajornaGameStore.DataAccess/bin/$BuildConfiguration/*" -DestinationPath "$ArtifactStagingDirectory/MajornaGameStore/MajornaGameStore.DataAccess/dataaccess.zip" -Force

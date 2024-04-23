afcff2a9cf0e8c832c97227d9c30a830f7db6aa6
xplizy <michel-partanen@gotmail.com>
2024-04-23 13:21:44 +0200

    ny design

--- Api/MajornaGameStore.DataAccess/Mongo/OrderRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using MajornaGameStore.DataAccess.Entities;
-using MajornaGameStore.Shared.Interfaces.RepositoryInterfaces;
-
-namespace MajornaGameStore.DataAccess.Mongo;
-
-public class OrderRepository : MongoRepositoryBase<Order>, IOrderRepository
-{
-    
-}

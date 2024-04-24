1f7dfe1b4b9c341cdb7f326b2c8bcebe7b52cffd
Arwin Rashid <arwin.rashid@iths.se>
2024-04-24 08:20:04 +0000

    Updated Program.cs

--- MajornaGameStore/MajornaGameStore.Api/Program.cs
+++ MajornaGameStore/MajornaGameStore.Api/Program.cs
@@ -98,12 +98,20 @@ builder.Services.AddScoped<MajornaGameStore.Api.Stripe.StripeClient>();
 builder.Services.AddCors(
     options => options.AddPolicy(
         name: "MyAllowSpecificOrigins",
+        policy => policy.WithOrigins("https://majornaggapi.azurewebsites.net", "https://majornagamestore.azurewebsites.net")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()));
+
+/* builder.Services.AddCors(
+    options => options.AddPolicy(
+        name: "MyAllowSpecificOrigins",
         policy => policy.WithOrigins("https://majornagamestore.azurewebsites.net", "https://majornaggapi.azurewebsites.net", [builder.Configuration["BackendUrl"] ?? "http://localhost:5102",
             builder.Configuration["FrontendUrl"] ?? "http://localhost:5241"])
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials()));
-
+*/
 
 
 
